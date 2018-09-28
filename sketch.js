7953398559ffe8f105b9f7c34fa281bee22922bb
Otavio Primo <otavioprimo@hotmail.com>
2018-09-28 19:09:52 -0300

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,32 @@
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '2401113739913777',
+    autoLogAppEvents: true,
+    xfbml: true,
+    version: 'v3.1'
+  });
+
+  FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) { return; }
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+function loginFacebook() {
+  FB.login(function (response) {
+    if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function (response) {
+        console.log('Good to see you, ' + response.name + '.');
+      });
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  });
+}
